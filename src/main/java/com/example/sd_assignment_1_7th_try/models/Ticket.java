ee9365e015ac080d2033018c8d06d3568ef86227
root <root@DESKTOP-STNSJN9.localdomain>
2023-03-12 01:44:26 +0200

    Create Ticket Model

---
+++ src/main/java/com/example/sd_assignment_1_7th_try/models/Ticket.java
@@ -0,0 +1,30 @@
+package com.example.sd_assignment_1_7th_try.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tickets")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "show_id", nullable = false)
+    private Show show;
+
+    @Column(name = "unit_price")
+    private Double unitPrice;
+
+    @Column(name = "places")
+    private Integer places;
+
+}
